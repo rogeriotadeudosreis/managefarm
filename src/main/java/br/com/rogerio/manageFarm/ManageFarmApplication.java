4224a8ef9b4e4675efbf5c6659118c243d464ec0
rogerioTadeuDosReis <rogerio.rtr46@gmail.com>
2022-05-11 09:41:24 -0300

    Actuator
    Tentativa de acrescentar o actuator na aplicação, mas o pom não aceitou, aguardando instrução do professor de como resolver

--- src/main/java/br/com/rogerio/manageFarm/ManageFarmApplication.java
+++ src/main/java/br/com/rogerio/manageFarm/ManageFarmApplication.java
@@ -3,7 +3,6 @@ package br.com.rogerio.manageFarm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
@@ -11,7 +10,6 @@ public class ManageFarmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ManageFarmApplication.class, args);
-        System.out.println("Senha Criptografada: " + new BCryptPasswordEncoder().encode("123456"));
     }
 
 }
